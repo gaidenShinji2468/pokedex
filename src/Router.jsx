cdca33b230f8aa7a70f2f187ff03fb2fae422abc
gaidenshinji <gaidenshinji2468@gmail.com>
2023-01-19 22:29:35 -0400

    Instaladas las dependencias; configuracion de redux preparada

---
+++ src/Router.jsx
@@ -0,0 +1,12 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+function Router()
+{
+
+}
+
+export default Router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Router from "./Router";
+import store from "./store";
+import {Provider} from "react-redux";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+      <Provider store={store}>
+	  <Router/>
+      </Provider>
   </React.StrictMode>,
 )
